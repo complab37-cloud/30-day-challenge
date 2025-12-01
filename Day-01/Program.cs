00b4eec98629a95d46f75dea8693bcb1510ccf41
Andrius Ivanauskas <andrius@vingesterminalas.lt>
2025-12-01 11:27:31 +0200

    Santa’s Tiling Trouble

--- Day-01/Program.cs
+++ Day-01/Program.cs
@@ -1,5 +1,12 @@
-﻿int a = 10, b = 11;
+﻿//Kadangi kambario matmenys duotyje nebuvo nurodyti, juos įvedžiau pats
+int length = 6; //kambario ilgis metrais
+int width = 3; //kambario plotis metrais
+double m2price = 21.4; //1m2
 
-Console.WriteLine("Hello, Day-01! " + (a+b));
+int area = length * width; //plotas
+
+double totalPrice= (area* m2price) * 1.05; //kaina ivertinus papildomus 5%
+
+Console.WriteLine($"Total price: {totalPrice}.");
 
 Console.ReadLine();
