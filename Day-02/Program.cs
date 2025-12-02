839ed420110d93bf1a3b84cecb18e3ffef28aafd
complab37 <complab37@gmail.com>
2025-12-02 10:29:21 +0200

    Santa’s Midnight Clock Countdown

---
+++ Day-02/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = 3, y = 5; //x - valandos, y - minutes
+
+int m = (x * 60) + y; //valandas ir minutes verciam i minutes
+int s = m * 60; //minutes verciam i sekundes
+
+Console.WriteLine($"Minutes passed: {m}" );
+Console.WriteLine($"Second passed: {s}" );
+
+Console.ReadLine();
