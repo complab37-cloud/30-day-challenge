a408cecad595d96b2a5ad2e07d4c22e38cf4dfe5
complab37 <complab37@gmail.com>
2025-12-08 11:04:36 +0200

    Santa’s Sleigh Flight Schedule

---
+++ Day-06/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 23, b = 55, c = 14;
+
+int totalMinutes = a * 60 + b + c;
+
+totalMinutes %= 1440;
+
+int v = totalMinutes / 60;
+int m = totalMinutes % 60;
+
+Console.WriteLine($"Santa will land at: {v}:{m}");
+
+Console.ReadLine();
