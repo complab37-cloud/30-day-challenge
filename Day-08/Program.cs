a7b0d8f360e3f839e119607d94b860ae058e11e8
complab37 <complab37@gmail.com>
2025-12-08 12:49:15 +0200

    Christmas Lights Pattern Generator

---
+++ Day-08/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Input:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 0; i < N + 2; i++)
+{
+    Console.Write("# ");
+}
+Console.WriteLine();
+
+for (int row = 1; row <= N; row++)
+{
+    Console.Write("# "); 
+
+    for (int col = 1; col <= N; col++)
+    {
+        int sum = row + col;
+
+        if (sum % 3 == 0 && sum % 5 == 0)
+        {
+            Console.Write("G ");
+        }
+        else if (sum % 3 == 0)
+        {
+            Console.Write("T ");
+        }
+        else if (sum % 5 == 0)
+        {
+            Console.Write("S ");
+        }
+        else
+        {
+            Console.Write(". ");
+        }
+    }
+
+    Console.WriteLine("#"); 
+}
+
+for (int i = 0; i < N + 2; i++)
+{
+    Console.Write("# ");
+}
+
+Console.ReadLine();   
