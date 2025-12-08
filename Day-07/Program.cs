822081fe42154408ca024c9e88da0b3067aecca9
complab37 <complab37@gmail.com>
2025-12-08 11:18:49 +0200

    Santa’s Christmas Concert Seating Plan

---
+++ Day-07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the number of rows:");
+int n  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number of seats in the first row:");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int s = n * (2 * k + (n - 1) * 2) / 2;
+
+Console.WriteLine($"The total number of seats: {s}");
+
+Console.ReadLine();
