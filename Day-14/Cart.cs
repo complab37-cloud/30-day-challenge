a652c5383b083185686e0cbfc25b292de6be3b62
complab37 <complab37@gmail.com>
2025-12-15 15:20:32 +0200

    Santa’s Gift Cart Engine

---
+++ Day-14/Cart.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Day_14
+{
+    public class Cart
+    {
+        List<string> ids = new List<string>();
+        List<decimal> prices = new List<decimal>();
+        decimal discount = 0;
+
+        public void Add(string id, decimal price)
+        {
+            if (price < 0)
+            {
+                return;
+            }
+            if (ids.Contains(id))
+            {
+                return;
+            }
+
+            ids.Add(id);
+            prices.Add(price);
+        }
+
+        public void Remove(string id)
+        {
+            int index = ids.IndexOf(id);
+            if (index >= 0)
+            {
+                ids.RemoveAt(index);
+                prices.RemoveAt(index);
+            }
+        }
+
+        public void ApplyDiscount(string code)
+        {
+            if (code == "PROMO10")
+            {
+                discount = 0.10m;
+            }
+            else if (code == "PROMO25")
+            {
+                discount = 0.25m;
+            }
+            else if (code == "SANTA50")
+            {
+                discount = 0.50m;
+            }
+        }
+
+        public decimal Total()
+        {
+            decimal sum = 0;
+            for (int i = 0; i < prices.Count; i++)
+            {
+                sum += prices[i];
+            }
+
+            return Math.Round(sum * (1 - discount), 2);
+        }
+
+        public void Clear()
+        {
+            ids.Clear();
+            prices.Clear();
+            discount = 0;
+        }
+    }
+}
---
+++ Day-14/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day_14;
+
+Cart giftCart = new Cart();
+
+giftCart.Add("train", 30);
+giftCart.Add("doll", 20);
+
+giftCart.ApplyDiscount("PROMO10");
+Console.WriteLine(giftCart.Total()); // 45.00
+
+giftCart.Remove("train");
+Console.WriteLine(giftCart.Total()); // 18.00
+
+
+Console.ReadLine();
