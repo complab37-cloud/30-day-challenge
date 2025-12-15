7009010a77b620b15a705ca89831370645c1c12c
complab37 <complab37@gmail.com>
2025-12-15 11:58:43 +0200

    Christmas School – Student Name Magic

---
+++ Day-13/Program.cs
@@ -0,0 +1,9 @@
+﻿string [] students = {"Petraitis Rokas", "Augė Artūras", "Mikalauskaitė Aušra", "Šlivka Donatas", "Stakėnaitė Ieva", "Skrėbė Domas", "Bruzgaitė Akvilė"};
+
+var girls = students.Where(s => !s.Split(' ')[1].EndsWith("as")).ToList();
+
+
+Console.WriteLine(girls.Count);
+girls.ForEach(Console.WriteLine);
+
+Console.ReadLine();
