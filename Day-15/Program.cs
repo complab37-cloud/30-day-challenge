f3c6b6d70e5bc38901596a1364cd4dcec6e79b47
complab37 <complab37@gmail.com>
2025-12-15 14:11:03 +0200

    Peter’s Christmas Shopping Dilemma

---
+++ Day-15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input three numbers separated by spaces and press enter:");
+double [] prices = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+double smallestPrice = prices.Min();
+
+Console.WriteLine();
+Console.WriteLine("Peter will spend {0:F2}",smallestPrice);
+
+Console.ReadLine();
