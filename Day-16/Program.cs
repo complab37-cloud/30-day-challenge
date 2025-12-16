350ff342cec84f0f8463d7b391e3ad7cf7b6265f
complab37 <complab37@gmail.com>
2025-12-16 10:40:11 +0200

    Santa’s Gift Audit

---
+++ Day-16/Program.cs
@@ -0,0 +1,27 @@
+﻿int[][] workshops =
+{
+    new int[] {5, 7, 3},    // Workshop 1: 3 elves
+    new int[] {6, 4, 4, 5}, // Workshop 2: 4 elves
+    new int[] {10, 2}       // Workshop 3: 2 elves
+};
+
+int total = 0;
+
+for (int i = 0; i < workshops.Length; i++)
+{
+    int workshopTotal = 0;
+
+    for (int j = 0; j < workshops[i].Length; j++)
+    {
+        workshopTotal += workshops[i][j];
+    }
+
+    total += workshopTotal;
+
+    Console.WriteLine($"Workshop {i+1} made {workshopTotal} gifts");
+}
+
+Console.WriteLine($"Santa’s total gift count is {total}");
+
+
+Console.ReadLine();
