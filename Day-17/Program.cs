321c98f80b3fb426c723ee08dcc8d71aee4e3d4b
complab37 <complab37@gmail.com>
2025-12-17 11:57:17 +0200

    Santa’s Magic Juice Bottling

---
+++ Day-17/Program.cs
@@ -0,0 +1,17 @@
+﻿int festiveBarrels, elfJugs, reindeerFlasks, remainder;
+
+int[] juiceAmounts = {45, 92, 33};
+
+foreach (var item in juiceAmounts)
+{
+    festiveBarrels = item / 5;  //5L
+    remainder = item % 5;       
+                                
+    elfJugs = remainder / 2;    //2L
+    remainder = remainder % 2;  
+                                
+    reindeerFlasks = remainder; //1L
+
+    Console.WriteLine($"{festiveBarrels} {elfJugs} {reindeerFlasks}");
+}
+Console.ReadLine();
