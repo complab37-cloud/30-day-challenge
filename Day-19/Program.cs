1929e079c9f62dbab1eac1ccd306691a75fc5f52
complab37 <complab37@gmail.com>
2025-12-19 13:22:51 +0200

    Santa’s Toy Collection – Trading Duplicates

---
+++ Day-19/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] a  = Console.ReadLine().Split(' ');
+int [] forTrade = new int[101];
+
+for (int i = 0; i < n; i++)
+{
+    forTrade[int.Parse(a[i])]++;
+}
+
+for (int j = 1; j <= 100; j++)
+{
+    if(forTrade[j] > 1)
+    {
+        Console.Write(j + " ");
+    }
+}
+
+Console.ReadLine();
